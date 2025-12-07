5039b8a56c4f8f6d3ace514984c62810ff625ae4
moriyaw <moriya@sodalabs.xyz>
2025-12-07 11:58:27 +0200

    Refactor import statement in userInteractor_pb2_grpc.py to use the correct module path for improved consistency.

--- proto/userInteractor_pb2_grpc.py
+++ proto/userInteractor_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-from core.proto import userInteractor_pb2 as userInteractor__pb2
+from proto import userInteractor_pb2 as userInteractor__pb2
 
 
 class UserInteractorServiceStub(object):
