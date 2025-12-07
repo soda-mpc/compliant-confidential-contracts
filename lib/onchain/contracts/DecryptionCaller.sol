ac4d904ddbc7319a0d1464bb6be7309edc3cba81
moriyaw <moriya@sodalabs.xyz>
2025-12-07 12:54:30 +0200

    Update import path in DecryptionCaller.sol for correct module resolution and consistency.

--- lib/onchain/contracts/DecryptionCaller.sol
+++ lib/onchain/contracts/DecryptionCaller.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.19;
 
 import "MpcCore.sol";
-import "GCDecryptionVerifierAddress.sol";
+import "../../../contracts/GCDecryptionVerifierAddress.sol";
 
 interface GCDecryptionVerifier {
     function verifyDecryptionSignatures(
